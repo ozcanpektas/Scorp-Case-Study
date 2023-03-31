b973a49f05c59b984d9831d9f5af9d364299379a
ozcanpektas <pktsozcan@gmail.com>
2023-03-31 15:25:11 +0300

    Initial Commit

---
+++ Case Study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Case Study
+//
+//  Created by Yusuf Pektaş on 31.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
