baf889c7d20a54cd3cb89cf4c12584507e786938
ozcanpektas <pktsozcan@gmail.com>
2023-03-31 18:59:09 +0300

    add tableView and refresh mech

--- Case Study/ViewController.swift
+++ Case Study/ViewController.swift
@@ -7,13 +7,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var people = [Person]()
+    var refreshController = UIRefreshControl()
+    var fNext: String? = nil
+
+    lazy var infoLabel: UILabel = {
+        var _infoLabel: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        _infoLabel.text = "No one here :)"
+        _infoLabel.isHidden = false
+        _infoLabel.textAlignment = .center
+        return _infoLabel
+    }()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(people[indexPath.row].fullName)- (\(people[indexPath.row].id)) --- \(indexPath.row+1)"
+        return cell
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.addSubview(infoLabel)
+        infoLabel.center = self.view.center
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        refreshController.addTarget(self, action: #selector(refreshData), for: UIControl.Event.valueChanged)
+        tableView.addSubview(refreshController)
+       
+    }
+    
+    @objc func refreshData() {
+        
+        DataSource.fetch(next: self.fNext) { response, error in
+            if let _ = response {
+                print("------------- sonraki responsee geldiiii \(response?.people.count) --------------")
+                response?.people.forEach({ person in
+                    self.people.append(person)
+                })
+                self.tableView.reloadData()
+                self.refreshController.endRefreshing()
+                
+                if !self.people.isEmpty {
+                    self.infoLabel.isHidden = true
+                }
+                
+            }
+            if let _ = error {
+                print("errrorrr happened")
+                self.tableView.reloadData()
+                self.refreshController.endRefreshing()
+            }
+        }
+        
     }
 
-
+    
 }
 
